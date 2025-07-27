9b1a679ba6069faf329a2b2bac95bdc9ec719beb
Todd Foust <toddfoust@outlook.com>
2025-07-27 11:05:24 -0400

    [AgentDiagnosticsFramework] Adding file header

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Diagnostics/AzureMonitorDiagnosticsEventListenerManager.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Diagnostics/AzureMonitorDiagnosticsEventListenerManager.cs
@@ -1,9 +1,5 @@
-﻿// <copyright file="AzureMonitorDiagnosticsListenerManager.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
-// </copyright>
-
-using System;
-using Azure.Monitor.OpenTelemetry.Exporter.Internals.Diagnostics;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 namespace Azure.Monitor.OpenTelemetry.Exporter.Internals.Diagnostics
 {
