4641a82e7c06800b09e49ed014331898a8fd0706
Todd Foust <toddfoust@outlook.com>
2025-07-27 01:30:25 -0400

    adding partial default

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/AzureMonitorOpenTelemetryExporterContext.Default.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Monitor.OpenTelemetry.Exporter
+{
+    public partial class AzureMonitorOpenTelemetryExporterContext
+    {
+        /// <summary>
+        /// Gets the default instance of <see cref="AzureMonitorOpenTelemetryExporterContext"/>.
+        /// </summary>
+        public static AzureMonitorOpenTelemetryExporterContext Default { get; } = new AzureMonitorOpenTelemetryExporterContext();
+    }
+}
